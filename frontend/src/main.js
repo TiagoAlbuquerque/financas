4c2020b20d59bdc1a4bbc764726e34ea495774d7
Tiago Oliveira de Albuquerque <4625297+TiagoAlbuquerque@users.noreply.github.com>
2026-06-26 14:00:14 -0300

    bump: set application version to 0.0.1

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -11,7 +11,7 @@ if (savedTheme === 'light') {
 }
 
 // Current App Version
-const AppVersion = "1.0.0";
+const AppVersion = "0.0.1";
 
 // Register Chart.js components
 Chart.register(...registerables);
